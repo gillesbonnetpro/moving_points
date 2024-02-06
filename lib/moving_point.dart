e13394629e8301678ffc33a30d8488f69bee6b6b
GB <gilles.bonnet.pro@gmail.com>
2024-02-06 10:52:39 +0100

    point cerclés de noir

--- lib/moving_point.dart
+++ lib/moving_point.dart
@@ -22,8 +22,11 @@ class _MovingPointState extends State<MovingPoint> {
         height: MediaQuery.of(context).size.shortestSide / 50,
         width: MediaQuery.of(context).size.shortestSide / 50,
         child: Container(
-          decoration:
-              BoxDecoration(color: widget.data.color, shape: BoxShape.circle),
+          decoration: BoxDecoration(
+            color: widget.data.color,
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.black),
+          ),
         ),
       ),
     );
